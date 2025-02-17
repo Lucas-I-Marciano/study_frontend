69b2c5a80b8b317937164f651e9529e8931a4886
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-02-17 12:33:27 -0300

    :books: docs: Notes from shift and unshift method

--- 04.logicProgramming/array.js
+++ 04.logicProgramming/array.js
@@ -16,3 +16,9 @@ console.log(boysAndGirls); // [ 'joao', 'tiago', 'sidemar', 'graciele', 'Lucas' 
 // pop
 boysAndGirls.pop();
 console.log(boysAndGirls); // [ 'joao', 'tiago', 'sidemar', 'graciele' ] -> Remove last item
+
+// shift and unshift
+boysAndGirls.shift();
+console.log(boysAndGirls); // [ 'tiago', 'sidemar', 'graciele' ] -> Remove first item
+boysAndGirls.unshift("joao");
+console.log(boysAndGirls); // [ 'joao', 'tiago', 'sidemar', 'graciele' ] -> Add on first postion
