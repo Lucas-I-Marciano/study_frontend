890aad142ae61f7ab6dfb86dc972deccc05d21dc
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-02-17 12:38:52 -0300

    :books: docs: Notes from splice and slice method

--- 04.logicProgramming/array.js
+++ 04.logicProgramming/array.js
@@ -22,3 +22,12 @@ boysAndGirls.shift();
 console.log(boysAndGirls); // [ 'tiago', 'sidemar', 'graciele' ] -> Remove first item
 boysAndGirls.unshift("joao");
 console.log(boysAndGirls); // [ 'joao', 'tiago', 'sidemar', 'graciele' ] -> Add on first postion
+
+// slice and splice
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+const sliced = fruits.slice(1);
+console.log(sliced); // [ 'Orange', 'Apple', 'Mango' ]
+fruits.splice(1, 0, "Watermellon", "Kiwi");
+console.log(fruits); // [ 'Banana', 'Watermellon', 'Kiwi', 'Orange', 'Apple', 'Mango' ]
+fruits.splice(0, 2);
+console.log(fruits); // [ 'Banana', 'Watermellon', 'Kiwi', 'Orange', 'Apple', 'Mango' ]
