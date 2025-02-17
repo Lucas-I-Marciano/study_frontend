5afa0b1d577e6ab3d9fa8f74e1f1833d76bd1b4f
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-02-17 12:46:01 -0300

    :books: docs: Notes from map method

--- 04.logicProgramming/array-iterations.js
+++ 04.logicProgramming/array-iterations.js
@@ -5,3 +5,11 @@ const users = ["joao", "tiago", "luka", "rodrigo"];
 users.forEach((user, index) => {
   console.log(index, user);
 });
+
+// map
+
+const arrayNumbers = [1, 5, 7, 2, 9];
+const result = arrayNumbers.map((value) => {
+  return value * 10;
+});
+console.log(result); // [ 10, 50, 70, 20, 90 ] -> It return a new array
