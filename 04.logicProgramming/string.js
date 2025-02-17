6f7933899b2c9422904418df366afae1f928402e
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-02-17 12:11:40 -0300

    :books: docs: Notes from slice and substring method

---
+++ 04.logicProgramming/string.js
@@ -0,0 +1,12 @@
+// Methods to extract part of string
+// slice, substring, substr
+
+// slice & substring
+const myName = "Lucas";
+console.log(myName.slice(1, 4)); // uca
+
+const fruits = "Apple, Banana, Kiwi";
+const kiwi = fruits.slice(15, 99);
+const kiwi2 = fruits.substring(15);
+console.log(kiwi); // Kiwi
+console.log(kiwi2); // Kiwi
