8cafd09d1f225a4a43b99fddeb4586ab030a6ca9
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-02-18 17:28:55 -0300

    :books: docs: Adding fetch exercice 2
    POST to create post

--- 05.webDeveloper/fetch.js
+++ 05.webDeveloper/fetch.js
@@ -5,3 +5,16 @@ const response = await fetch("https://jsonplaceholder.typicode.com/posts");
 const data = await response.json();
 
 console.log(data[0]);
+
+// create post
+const body = {
+  title: "First post",
+  body: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusantium ullam culpa in",
+};
+
+const response2 = await fetch("https://jsonplaceholder.typicode.com/posts", {
+  method: "post",
+  body: JSON.stringify(body),
+});
+const data2 = await response2.json();
+console.log(data2); // { id: 101 }
