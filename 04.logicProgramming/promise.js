027fe7b01887a0ad2d3a224be18159731a7eafc2
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-02-18 12:35:47 -0300

    :books: docs: Running two promises
    But second promise will run after first was resolved

--- 04.logicProgramming/promise.js
+++ 04.logicProgramming/promise.js
@@ -3,11 +3,23 @@
 const p1 = () => {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      reject("this promise was NOT resolved!");
+      resolve("this first promise was resolved!");
+    }, 1500);
+  });
+};
+
+const p2 = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("this second promise was resolved!");
     }, 1500);
   });
 };
 
 p1()
-  .then((data) => console.log(data))
-  .catch((error) => console.error(error)); // this promise was resolved
+  .then((data) => {
+    console.log(data);
+    p2().then((data2) => console.log(data2));
+  })
+  .catch((error) => console.error(error))
+  .finally(() => console.log("End of p1!")); // this first promise was resolved! || End of p1! || this second promise was resolved!
