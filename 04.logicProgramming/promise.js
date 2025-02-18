b81a89237421c459f1f58f51a979e9fee52e2d86
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-02-18 12:24:02 -0300

    :books: docs: Simulating functions that takes time to resolve
    It will not return the resolved value because of console is running before promise was resolved

--- 04.logicProgramming/promise.js
+++ 04.logicProgramming/promise.js
@@ -2,8 +2,10 @@
 
 const p1 = () => {
   return new Promise((resolve, reject) => {
-    resolve("this promise was resolved");
+    setTimeout(() => {
+      resolve("this promise was resolved");
+    }, 3000);
   });
 };
 
-console.log(p1()); // Promise { 'this promise was resolved' }
+console.log(p1()); // Promise { <pending> }
