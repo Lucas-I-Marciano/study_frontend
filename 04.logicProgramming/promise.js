4070abbb7d9af57517c9d22d0a17a734a1d4940e
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-02-18 12:41:52 -0300

    :books: docs: Using async functions

--- 04.logicProgramming/promise.js
+++ 04.logicProgramming/promise.js
@@ -19,3 +19,10 @@ const p2 = () => {
 Promise.all([p1(), p2()]).then((data) => {
   return console.log(data);
 }); // ['this first promise was resolved!', 'this second promise was resolved!']
+
+// Async function
+async function asyncFunction() {
+  console.log(await p1());
+}
+
+asyncFunction(); // this first promise was resolved!
