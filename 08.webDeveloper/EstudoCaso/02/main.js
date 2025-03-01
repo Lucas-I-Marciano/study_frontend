64fa1fbd7d387fc4d3457852bfa55e1778deea07
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-03-01 15:36:22 -0300

    :books: study: Handle with error and throwing error

--- 08.webDeveloper/EstudoCaso/02/main.js
+++ 08.webDeveloper/EstudoCaso/02/main.js
@@ -1,13 +1,18 @@
 window.onload = () => {
   const form = document.querySelector("form");
   const ulElement = document.querySelector("ul");
+  let count = 0;
   form.addEventListener("submit", async (event) => {
+    count++;
     event.preventDefault();
     const taskTitle = event.target[0].value;
 
-    ulElement.innerHTML += `<li>${taskTitle}</li> `;
+    ulElement.innerHTML += `<li>${count}: ${taskTitle}</li> `;
 
     try {
+      if (count === 3) {
+        throw new Error("Three brings bad lucky");
+      }
       const response = await (
         await fetch("https://jsonplaceholder.typicode.com/todos", {
           method: "POST",
@@ -15,6 +20,9 @@ window.onload = () => {
         })
       ).json();
     } catch (error) {
+      const liArray = ulElement.querySelectorAll("li");
+      const lastLiElement = liArray[liArray.length - 1];
+      lastLiElement.remove();
       alert(error);
     }
   });
