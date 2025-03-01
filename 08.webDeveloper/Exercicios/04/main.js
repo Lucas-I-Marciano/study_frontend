99d811ed3e73b27e0d0fce9c3e0258cb5d33026d
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-02-28 21:12:32 -0300

    :books: study: Map each div to treat as a button

--- 08.webDeveloper/Exercicios/04/main.js
+++ 08.webDeveloper/Exercicios/04/main.js
@@ -1,3 +1,11 @@
 window.onload = () => {
   console.log("Loaded");
+
+  const rows = document.querySelectorAll(".calculator-pad-row");
+  const [e, mi, sin, deg] = rows[0].querySelectorAll("div");
+  const [ac, back, div, mult] = rows[1].querySelectorAll("div");
+  const [seven, eight, nine, minus] = rows[2].querySelectorAll("div");
+  const [four, five, six, plus] = rows[3].querySelectorAll("div");
+  const [one, two, three, equal] = rows[4].querySelectorAll("div");
+  const [, zero, dot] = rows[5].querySelectorAll("div");
 };
