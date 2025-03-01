a823ecf997fbeaab9c3ce7e2bef93db8cf76ffc4
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-03-01 16:10:05 -0300

    :books: study: Handle with fetch error more robust - It will remove the element

---
+++ 08.webDeveloper/EstudoCaso/02/main.js
@@ -0,0 +1,36 @@
+window.onload = () => {
+  const form = document.querySelector("form");
+  const ulElement = document.querySelector("ul");
+  let count = 0;
+  form.addEventListener("submit", async (event) => {
+    count++;
+    event.preventDefault();
+    const taskTitle = event.target[0].value;
+
+    const liElement = document.createElement("li");
+    liElement.innerText = `${count}: ${taskTitle}`;
+    ulElement.appendChild(liElement);
+
+    try {
+      if (count === 3) {
+        const response = await (
+          await fetch("https://jsonplaceholder.t.com/todos", {
+            method: "POST",
+            "Content-Type": "application/json",
+            body: JSON.stringify({ title: taskTitle }),
+          })
+        ).json();
+      }
+      const response = await (
+        await fetch("https://jsonplaceholder.typicode.com/todos", {
+          method: "POST",
+          "Content-Type": "application/json",
+          body: JSON.stringify({ title: taskTitle }),
+        })
+      ).json();
+    } catch (error) {
+      liElement.remove();
+      alert(error);
+    }
+  });
+};
