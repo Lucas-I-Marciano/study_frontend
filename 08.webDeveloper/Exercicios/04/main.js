0fb67bef7a7f2eb47f593461604383f84b87c896
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-02-28 21:22:41 -0300

    :books: study: Map each operation and if none of them concat number as string

--- 08.webDeveloper/Exercicios/04/main.js
+++ 08.webDeveloper/Exercicios/04/main.js
@@ -8,4 +8,30 @@ window.onload = () => {
   const [four, five, six, plus] = rows[3].querySelectorAll("div");
   const [one, two, three, equal] = rows[4].querySelectorAll("div");
   const [, zero, dot] = rows[5].querySelectorAll("div");
+
+  // prettier-ignore
+  const allButtons = [
+    e,mi,sin,deg,ac,back,div,mult,seven,eight,nine,minus,
+    four,five,six,plus,one,two,three,equal,zero,dot,
+  ];
+
+  let numberOne = "";
+  for (const element of allButtons) {
+    element.addEventListener("click", () => {
+      if (element === ac) {
+        numberOne = "";
+      } else if (element === div) {
+        console.log("Divide");
+      } else if (element === mult) {
+        console.log("Multiply");
+      } else if (element === plus) {
+        console.log("Sum");
+      } else if (element === minus) {
+        console.log("Subtract");
+      } else {
+        numberOne = `${numberOne}${element.innerText}`;
+        console.log(numberOne);
+      }
+    });
+  }
 };
