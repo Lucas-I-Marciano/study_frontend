2787c5e9685499834cd2f1eac54ecee27ef50338
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-03-04 08:53:47 -0300

    :sparkles: feat: Prepare form data to send contact data

--- 07.webDeveloper/EstudoCaso/src/contact.js
+++ 07.webDeveloper/EstudoCaso/src/contact.js
@@ -2,5 +2,15 @@ window.onload = () => {
   const form = document.querySelector("form");
   form.addEventListener("submit", (event) => {
     event.preventDefault();
+
+    const nameInput = event.target[0];
+    const emailInput = event.target[1];
+    const messageInput = event.target[2];
+
+    const data = {
+      name: nameInput.value,
+      email: emailInput.value,
+      message: messageInput.value,
+    };
   });
 };
