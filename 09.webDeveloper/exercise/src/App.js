60a2605b382028a6bb8244e877cc4675ca8d109c
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-03-05 12:06:22 -0300

    :books: study: Update app.js to show each article

--- 09.webDeveloper/exercise/src/App.js
+++ 09.webDeveloper/exercise/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import Main from "./components/Main/Main";
 
 function App() {
   return (
     <div className="App">
       <Header>Artigos Publicados</Header>
+      <Main />
       <Footer>Desenvolvido por Lucas M.</Footer>
     </div>
   );
