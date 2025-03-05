9ca3d14d62ecd8b677a55797a181907cc74decda
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-03-05 12:31:56 -0300

    :recycle: refactor: Change from hard code to loop

--- 09.webDeveloper/exercise/src/components/Main/Main.js
+++ 09.webDeveloper/exercise/src/components/Main/Main.js
@@ -1,17 +1,29 @@
 import Article from "../Article/Article";
 import "./Main.css";
 
+const createArticle = (title, begginingDescription = "") => {
+  return {
+    title,
+    description: `${begginingDescription}Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent elit leo, scelerisque non ornare tristique, euismod ac ante. Duis in iaculis felis, ac volutpat erat. Donec dignissim lectus vitae risus efficitur, ac ultricies dolor tempor. Etiam vitae risus ligula. Duis fringilla consectetur ligula sit amet malesuada. Mauris rutrum tempor pharetra. Curabitur ut lorem ipsum.`,
+  };
+};
+
+const articles = [
+  createArticle("Article 1", "1 - "),
+  createArticle("Article 2", "2 - "),
+  createArticle("Article 3", "3 - "),
+  createArticle("Article 4", "4 - "),
+  createArticle("Article 5", "Estudo sobre TCC como fazer amigos - "),
+];
+
 const Main = () => {
   return (
     <main className="main">
-      <Article
-        title="Article 1"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent elit leo, scelerisque non ornare tristique, euismod ac ante. Duis in iaculis felis, ac volutpat erat. Donec dignissim lectus vitae risus efficitur, ac ultricies dolor tempor. Etiam vitae risus ligula. Duis fringilla consectetur ligula sit amet malesuada. Mauris rutrum tempor pharetra. Curabitur ut lorem ipsum."
-      />{" "}
-      <Article
-        title="Article 2"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent elit leo, scelerisque non ornare tristique, euismod ac ante. Duis in iaculis felis, ac volutpat erat. Donec dignissim lectus vitae risus efficitur, ac ultricies dolor tempor. Etiam vitae risus ligula. Duis fringilla consectetur ligula sit amet malesuada. Mauris rutrum tempor pharetra. Curabitur ut lorem ipsum."
-      />{" "}
+      {articles.map((object) => {
+        return (
+          <Article title={object.title} description={object.description} />
+        );
+      })}
     </main>
   );
 };
