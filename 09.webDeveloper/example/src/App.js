ceb048715600b9255ed43f8313ede44f22aff84f
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-03-05 10:57:47 -0300

    :books: study: Use button component

--- 09.webDeveloper/example/src/App.js
+++ 09.webDeveloper/example/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Button from "./components/Button/Button";
 
 function App() {
   return (
     <div className="App">
       <h1>Meu nome é Lucas</h1>
       <p>Estou estudando react</p>
+      <Button />
     </div>
   );
 }
