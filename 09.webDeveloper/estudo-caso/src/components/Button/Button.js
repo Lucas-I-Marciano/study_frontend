3d3138dbe45d27148373bfe33d3f1fb73642eed3
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-03-06 17:39:54 -0300

    :books: study: Add another way of passing listeners
    It works with every property that has the same name on App and on Component

--- 09.webDeveloper/estudo-caso/src/components/Button/Button.js
+++ 09.webDeveloper/estudo-caso/src/components/Button/Button.js
@@ -1,13 +1,15 @@
 import "./Button.css";
 
 const Button = (props) => {
+  const { variant, disabled, isLoading, children, ...rest } = props;
+  console.log(rest); // {onClick: ƒ}
   return (
     <button
-      className={`button button__${props.variant}`}
-      disabled={props.disabled || props.isLoading}
-      onClick={props.onClick}
+      className={`button button__${variant}`}
+      disabled={disabled || isLoading}
+      {...rest}
     >
-      {props.isLoading ? "Carregando..." : props.children}
+      {isLoading ? "Carregando..." : children}
     </button>
   );
 };
