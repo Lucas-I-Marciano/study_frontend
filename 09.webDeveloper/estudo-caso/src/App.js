430a82d7fbac89bd8201925aa23c59fb3f06ca92
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-03-06 17:47:53 -0300

    :books: study: Change flex-direction to column to see buttons above text components

--- 09.webDeveloper/estudo-caso/src/App.js
+++ 09.webDeveloper/estudo-caso/src/App.js
@@ -1,20 +1,26 @@
 import "./App.css";
 import Button from "./components/Button/Button";
+import Text from "./components/Text/Text";
 
 function App() {
   return (
     <div className="container">
-      <Button variant="solid">Solid</Button>
-      <Button variant="outline">Outline</Button>
-      <Button variant="outline" isLoading>
-        Outline
-      </Button>
-      <Button variant="solid" disabled>
-        Solid
-      </Button>
-      <Button variant="solid" onClick={() => console.log("Click")}>
-        Click Solid
-      </Button>
+      <div className="container-buttons">
+        <Button variant="solid">Solid</Button>
+        <Button variant="outline">Outline</Button>
+        <Button variant="outline" isLoading>
+          Outline
+        </Button>
+        <Button variant="solid" disabled>
+          Solid
+        </Button>
+        <Button variant="solid" onClick={() => console.log("Click")}>
+          Click Solid
+        </Button>
+      </div>
+      <div className="container-text">
+        <Text>Olá</Text>
+      </div>
     </div>
   );
 }
