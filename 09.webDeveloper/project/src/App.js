babd8ceeca7eefcd1edca53f3e534024b51cfe81
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-03-07 13:10:57 -0300

    :sparkles: feat: Change content based on props

--- 09.webDeveloper/project/src/App.js
+++ 09.webDeveloper/project/src/App.js
@@ -1,11 +1,18 @@
 import "./App.css";
 import { Header } from "./components/Header/Header";
+import { Task } from "./components/Task/Task";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
         <Header>Kanban</Header>
+        <Task
+          title="Christmas Banners"
+          label="label"
+          colorScheme="todo"
+          avatarUrl="male"
+        ></Task>
       </div>
     </div>
   );
--- 09.webDeveloper/project/src/components/Task/Task.js
+++ 09.webDeveloper/project/src/components/Task/Task.js
@@ -1,13 +1,16 @@
 import "./Task.css";
+import logoMan from "../../assets/images/avatar/Male.png";
+import logoFem from "../../assets/images/avatar/Female.png";
 
 export const Task = (props) => {
   const { avatarUrl, title, label, colorScheme } = props;
+  const logo = avatarUrl === "male" ? logoMan : logoFem;
   return (
-    <div className="task">
-      <span className="task-title">Christmas Banners</span>
+    <div className={`task task__${colorScheme}`}>
+      <span className="task-title">{title}</span>
       <div className="task-flags">
-        <img className="task-flags-image" alt="avatar"></img>
-        <span className="task-flags-label">Label</span>
+        <img className="task-flags-image" src={logo} alt="avatar"></img>
+        <span className="task-flags-label">{label}</span>
       </div>
     </div>
   );
