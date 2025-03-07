e97d548f2a8c4cd2c4cc8be65ed0010cd1f009eb
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-03-07 13:33:56 -0300

    :sparkles: feat: Add style to label and send params on component

--- 09.webDeveloper/project/src/components/Task/Task.js
+++ 09.webDeveloper/project/src/components/Task/Task.js
@@ -6,11 +6,13 @@ export const Task = (props) => {
   const { avatarUrl, title, label, colorScheme } = props;
   const logo = avatarUrl === "male" ? logoMan : logoFem;
   return (
-    <div className={`task task__${colorScheme}`}>
+    <div className={`task`}>
       <span className="task-title">{title}</span>
       <div className="task-flags">
         <img className="task-flags-image" src={logo} alt="avatar"></img>
-        <span className="task-flags-label">{label}</span>
+        <span className={`task-flags-label task-flags-label__${colorScheme}`}>
+          {label}
+        </span>
       </div>
     </div>
   );
