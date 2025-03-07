2a1b19f77681d2816e12b2b11fae4054f6f9f8c7
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-03-07 17:56:27 -0300

    :sparkles: feat: Change color based on colorScheme props

--- 09.webDeveloper/project/src/App.js
+++ 09.webDeveloper/project/src/App.js
@@ -1,14 +1,27 @@
 import "./App.css";
 import { Card } from "./components/Card/Card";
 import { Header } from "./components/Header/Header";
-// import { Task } from "./components/Task/Task";
+import { Task } from "./components/Task/Task";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
         <Header>Kanban</Header>
-        <Card></Card>
+        <Card titleColor="white" colorScheme="blue">
+          <Task
+            title="Redo Portfolio"
+            label="label"
+            colorScheme="todo"
+            avatarUrl="male"
+          ></Task>
+          <Task
+            title="Task 1"
+            label="label"
+            colorScheme="todo"
+            avatarUrl="male"
+          ></Task>
+        </Card>
       </div>
     </div>
   );
--- 09.webDeveloper/project/src/components/Card/Card.js
+++ 09.webDeveloper/project/src/components/Card/Card.js
@@ -1,40 +1,24 @@
-import { Task } from "../Task/Task";
 import "./Card.css";
 
-export const Card = () => {
+export const Card = (props) => {
+  const { children, titleColor, colorScheme = "blue" } = props;
+  console.log(children);
   return (
-    <div className="card">
-      <div className="card-shadow"></div>
+    <div className={`card card__${colorScheme}`}>
+      <div className={`card-shadow card-shadow__${colorScheme}`}></div>
       <div className="card-content">
         <div className="card-content-title">
           <span className="card-content-title-icon">🗒️</span>
-          <span className="card-content-title-text">To Do</span>
+          <span
+            className={`card-content-title-text card-content-title-text__${titleColor}`}
+          >
+            To Do
+          </span>
         </div>
         <div className="card-content-task">
-          <Task
-            title="Christmas Banners"
-            label="label"
-            colorScheme="todo"
-            avatarUrl="male"
-          ></Task>
-          <Task
-            title="Redo Portfolio"
-            label="label"
-            colorScheme="todo"
-            avatarUrl="male"
-          ></Task>
-          <Task
-            title="Clean code"
-            label="label"
-            colorScheme="todo"
-            avatarUrl="male"
-          ></Task>
-          <Task
-            title="Fix layout"
-            label="label"
-            colorScheme="todo"
-            avatarUrl="male"
-          ></Task>
+          {children.map((i) => {
+            return i;
+          })}
         </div>
       </div>
     </div>
