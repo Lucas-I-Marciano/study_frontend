e4972f3583b2aa462eabf2864ddcc5aaf716a537
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-03-08 10:55:58 -0300

    :recycle: refactor: Change from Card to KanbanTable component on App.js

--- 09.webDeveloper/project/src/App.js
+++ 09.webDeveloper/project/src/App.js
@@ -1,26 +1,13 @@
 import "./App.css";
-import { Card } from "./components/Card/Card";
 import { Header } from "./components/Header/Header";
-import { dbTask } from "./components/Task/dbTask";
-import { Task } from "./components/Task/Task";
+import { KanbanTable } from "./components/KanbanTable/KanbanTable";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
         <Header>Kanban</Header>
-        <Card titleText="To do" titleColor="black" colorScheme="blue" icon="✔️">
-          {dbTask.map((element) => {
-            return (
-              <Task
-                title={element.title}
-                label={element.label}
-                colorScheme={element.colorScheme}
-                avatarUrl={element.avatarUrl}
-              ></Task>
-            );
-          })}
-        </Card>
+        <KanbanTable />
       </div>
     </div>
   );
