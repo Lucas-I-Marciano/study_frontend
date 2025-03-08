3d0c479a08ae9904f2f91d0b56fd04fab16dbcdf
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-03-08 10:55:17 -0300

    :sparkles: feat: Create KanbanTable component importing Card and Task

---
+++ 09.webDeveloper/project/src/components/KanbanTable/KanbanTable.js
@@ -0,0 +1,23 @@
+import { Card } from "../Card/Card";
+import { dbTask } from "../Task/dbTask";
+import { Task } from "../Task/Task";
+import "./KanbanTable.css";
+
+export const KanbanTable = () => {
+  return (
+    <div className="KanbanTable">
+      <Card titleText="To do" titleColor="black" colorScheme="blue" icon="✔️">
+        {dbTask.map((element) => {
+          return (
+            <Task
+              title={element.title}
+              label={element.label}
+              colorScheme={element.colorScheme}
+              avatarUrl={element.avatarUrl}
+            ></Task>
+          );
+        })}
+      </Card>
+    </div>
+  );
+};
