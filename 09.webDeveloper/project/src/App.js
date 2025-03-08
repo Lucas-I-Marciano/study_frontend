0c63b61b2c5bf5b609d63f697309717b8556fa11
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-03-07 23:43:04 -0300

    :sparkles: feat: Change from hard code to a simulated DB

--- 09.webDeveloper/project/src/App.js
+++ 09.webDeveloper/project/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { Card } from "./components/Card/Card";
 import { Header } from "./components/Header/Header";
+import { dbTask } from "./components/Task/dbTask";
 import { Task } from "./components/Task/Task";
 
 function App() {
@@ -9,18 +10,16 @@ function App() {
       <div className="container">
         <Header>Kanban</Header>
         <Card titleText="To do" titleColor="black" colorScheme="blue" icon="✔️">
-          <Task
-            title="Redo Portfolio"
-            label="label"
-            colorScheme="todo"
-            avatarUrl="male"
-          ></Task>
-          <Task
-            title="Task 1"
-            label="label"
-            colorScheme="todo"
-            avatarUrl="male"
-          ></Task>
+          {dbTask.map((element) => {
+            return (
+              <Task
+                title={element.title}
+                label={element.label}
+                colorScheme={element.colorScheme}
+                avatarUrl={element.avatarUrl}
+              ></Task>
+            );
+          })}
         </Card>
       </div>
     </div>
---
+++ 09.webDeveloper/project/src/components/Task/dbTask.js
@@ -0,0 +1,20 @@
+export const dbTask = [
+  {
+    title: "Redo Portfolio",
+    label: "study",
+    colorScheme: "blue",
+    avatarUrl: "male",
+  },
+  {
+    title: "Task 1",
+    label: "bank",
+    colorScheme: "blue",
+    avatarUrl: "male",
+  },
+  {
+    title: "Task 2",
+    label: "bank",
+    colorScheme: "blue",
+    avatarUrl: "female",
+  },
+];
