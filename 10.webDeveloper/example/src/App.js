34203e7de3cdb4d074b5b847b9317de4b398e5d2
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-03-10 12:37:21 -0300

    :books: study: Add Button component with children text

--- 10.webDeveloper/example/src/App.js
+++ 10.webDeveloper/example/src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import { Button } from "./components/Button/Button";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Button>Meu botão</Button>
+    </div>
+  );
 }
 
 export default App;
