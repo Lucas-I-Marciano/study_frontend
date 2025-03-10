b0878b109c13b77d1703b598d36f94a8913a2c10
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-03-10 19:49:15 -0300

    :books: study: Add use case of useEffect
    Take note that it will change external resorce - 'document.title'

--- 10.webDeveloper/example/src/App.js
+++ 10.webDeveloper/example/src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
 import { Button } from "./components/Button/Button";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    document.title = count;
+  }, [count]);
   return (
     <div className="App">
       <p>O Botão foi clicado {count} vezes</p>
