eb7dd1372e167f6c885173196f3341b45fa3bcb1
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-03-10 12:39:03 -0300

    :books: study: Use Hook of state to control <p> text

--- 10.webDeveloper/example/src/App.js
+++ 10.webDeveloper/example/src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
 import { Button } from "./components/Button/Button";
 
+import { useState } from "react";
+
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <div className="App">
+      <p>O Botão foi clicado {count} vezes</p>
       <Button>Meu botão</Button>
     </div>
   );
