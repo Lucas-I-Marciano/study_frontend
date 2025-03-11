37e1b9284590609861af62c26c90b839a8d9091b
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-03-11 20:36:34 -0300

    :books: study: Add query function to get data from jsonplaceholder

--- 10.webDeveloper/exercise/src/App.js
+++ 10.webDeveloper/exercise/src/App.js
@@ -1,7 +1,27 @@
 import "./App.css";
+import { Button } from "./Button/Button";
+
+const query = async (endpoint) => {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/${endpoint}`
+  );
+  const responseJson = await response.json();
+  console.log(responseJson);
+  return responseJson;
+};
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Button
+        onClick={() => {
+          query("users");
+        }}
+      >
+        Click
+      </Button>
+    </div>
+  );
 }
 
 export default App;
