a36bfd8cc6e01dc19686de92b05c9aee6b17e735
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-03-13 18:44:40 -0300

    :books: study: Get box context and its dispacth function to control state

--- 10.webDeveloper/estudo-caso/src/components/Box.js
+++ 10.webDeveloper/estudo-caso/src/components/Box.js
@@ -1,9 +1,20 @@
+import { useBox } from "../context/box-context";
+
 export const Box = (props) => {
-  const { items } = props;
+  const boxContext = useBox();
+  const { items, boxId } = props;
   return (
     <div className="box">
       {items.map((button) => {
-        return <button>{button.name}</button>;
+        return (
+          <button
+            onClick={() => {
+              boxContext.dispatch({ type: `box-${boxId}` });
+            }}
+          >
+            {button.name}
+          </button>
+        );
       })}
     </div>
   );
