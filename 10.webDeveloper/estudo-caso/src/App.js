e15213f508f16b166171a54d207c5242a759f047
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-03-13 18:00:04 -0300

    :books: study: Implement Box component on App

--- 10.webDeveloper/estudo-caso/src/App.js
+++ 10.webDeveloper/estudo-caso/src/App.js
@@ -1,18 +1,42 @@
 import "./App.css";
+import { Box } from "./components/Box";
+
+const initialState = {
+  firstBox: [
+    {
+      id: 0,
+      name: "button 0",
+    },
+    {
+      id: 1,
+      name: "button 1",
+    },
+    {
+      id: 2,
+      name: "button 2",
+    },
+  ],
+  secondBox: [
+    {
+      id: 3,
+      name: "button 3",
+    },
+    {
+      id: 4,
+      name: "button 4",
+    },
+    {
+      id: 5,
+      name: "button 5",
+    },
+  ],
+};
 
 function App() {
   return (
     <div className="container">
-      <div className="box">
-        <button>Item 1</button>
-        <button>Item 2</button>
-        <button>Item 3</button>
-      </div>
-      <div className="box">
-        <button>Item 4</button>
-        <button>Item 5</button>
-        <button>Item 6</button>
-      </div>
+      <Box items={initialState["firstBox"]}></Box>
+      <Box items={initialState["secondBox"]}></Box>
     </div>
   );
 }
