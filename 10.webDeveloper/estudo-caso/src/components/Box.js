30ffc23c3b93efef6fbce7063bba61e51b1c717b
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-03-13 17:59:49 -0300

    :books: study: Add Box component to hold each button

---
+++ 10.webDeveloper/estudo-caso/src/components/Box.js
@@ -0,0 +1,11 @@
+export const Box = (props) => {
+  const { items } = props;
+  return (
+    <div className="box">
+      {items.map((button) => {
+        return <button>{button.name}</button>;
+      })}
+      ;
+    </div>
+  );
+};
