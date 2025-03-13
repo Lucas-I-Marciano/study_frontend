75dcec58ea498597340fbefbaa2185cddf961fed
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-03-13 17:53:56 -0300

    :books: study: Add two box to simulate behavior
    It is expected that when click on item from one box move to the other

--- 10.webDeveloper/estudo-caso/src/App.js
+++ 10.webDeveloper/estudo-caso/src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="container">
+      <div className="box">
+        <button>Item 1</button>
+        <button>Item 2</button>
+        <button>Item 3</button>
+      </div>
+      <div className="box">
+        <button>Item 4</button>
+        <button>Item 5</button>
+        <button>Item 6</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
