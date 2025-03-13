3a5fea1cb7a45744abad41ce8b3ee6a918b77c68
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-03-13 18:27:01 -0300

    :recycle: refactor: Refactor to use global context

--- 10.webDeveloper/estudo-caso/src/App.js
+++ 10.webDeveloper/estudo-caso/src/App.js
@@ -1,42 +1,13 @@
 import "./App.css";
-import { Box } from "./components/Box";
-
-const initialState = {
-  firstBox: [
-    {
-      id: 0,
-      name: "button 0",
-    },
-    {
-      id: 1,
-      name: "button 1",
-    },
-    {
-      id: 2,
-      name: "button 2",
-    },
-  ],
-  secondBox: [
-    {
-      id: 3,
-      name: "button 3",
-    },
-    {
-      id: 4,
-      name: "button 4",
-    },
-    {
-      id: 5,
-      name: "button 5",
-    },
-  ],
-};
+import { BoxList } from "./components/BoxList";
+import { BoxProvider } from "./context/box-context";
 
 function App() {
   return (
-    <div className="container">
-      <Box items={initialState["firstBox"]}></Box>
-      <Box items={initialState["secondBox"]}></Box>
+    <div>
+      <BoxProvider>
+        <BoxList></BoxList>
+      </BoxProvider>
     </div>
   );
 }
