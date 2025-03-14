eaeda58bfd84e40c9ee0e4565e32923030496bb7
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-03-14 13:08:24 -0300

    :sparkles: feat: Use global query context Provider

--- 10.webDeveloper/estudo-caso/src/App-query.js
+++ 10.webDeveloper/estudo-caso/src/App-query.js
@@ -1,15 +1,20 @@
 import "./App-query.css";
 import { Button } from "./components/Button";
+import { Span } from "./components/Span";
+import { QueryProvider } from "./context/query-context";
 
 function AppQuery() {
   return (
     <div>
-      <div className="button-list">
-        <Button endpoint="todos">To do</Button>
-        <Button endpoint="users">Users</Button>
-        <Button endpoint="posts">Posts</Button>
-        <Button endpoint="posasdts">Posts</Button>
-      </div>
+      <QueryProvider>
+        <div className="button-list">
+          <Button endpoint="todos">To do</Button>
+          <Button endpoint="users">Users</Button>
+          <Button endpoint="posts">Posts</Button>
+          <Button endpoint="posasdts">Posts</Button>
+        </div>
+        <Span>Resultado</Span>
+      </QueryProvider>
     </div>
   );
 }
