b157553b6f12ab84ed611e2a99e0b07c2641d901
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-03-13 21:21:39 -0300

    :recycle: refactor: Import AppQuery on index

--- 10.webDeveloper/estudo-caso/src/index.js
+++ 10.webDeveloper/estudo-caso/src/index.js
@@ -2,10 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import AppQuery from "./App-query";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <AppQuery />
   </React.StrictMode>
 );
