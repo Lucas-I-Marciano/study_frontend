e29dfae47631d326c4d6fea1d2f0c5e39c4dce4c
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-03-14 07:41:04 -0300

    :sparkles: feat: Add fetchData function to query API and dispatch function to hold its requisition state

--- 10.webDeveloper/estudo-caso/src/components/Button.js
+++ 10.webDeveloper/estudo-caso/src/components/Button.js
@@ -4,8 +4,8 @@ const reducer = (state, action) => {
   switch (action.type) {
     case "loading":
       return "loading";
-    case "consulting":
-      return "consulting";
+    case "success":
+      return "success";
     case "failed":
       return "failed";
     case "idle":
@@ -16,6 +16,16 @@ const reducer = (state, action) => {
   }
 };
 
+const fetchData = async (endpoint) => {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/${endpoint}`
+  );
+  const jsonResponse = await response.json();
+  return jsonResponse;
+
+  // return jsonResponse;
+};
+
 export const Button = (props) => {
   const [state, dispatch] = useReducer(reducer, "idle");
   const { children, endpoint } = props;
@@ -23,7 +33,19 @@ export const Button = (props) => {
     <div>
       <button
         onClick={async () => {
-          dispatch({ type: "failed" });
+          dispatch({ type: "loading" });
+          try {
+            const response = await fetchData(endpoint);
+            setTimeout(() => {
+              if (Object.keys(response).length !== 0) {
+                dispatch({ type: "success" });
+              } else {
+                dispatch({ type: "failed" });
+              }
+            }, 500);
+          } catch {
+            dispatch({ type: "failed" });
+          }
         }}
       >
         {children} - {state}
