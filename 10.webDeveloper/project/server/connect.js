40a45abe56a31d3a86cdbb198915b1c7b39da3af
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-03-15 16:11:09 -0300

    :sparkles: feat: Create connection with mongoDB database
    Two collections will guide the project - 'shoe' and 'liked_shoe'

---
+++ 10.webDeveloper/project/server/connect.js
@@ -0,0 +1,25 @@
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+
+dotenv.config(); // Carregar variáveis de ambiente
+
+const db_string = process.env.ATLAS_URI;
+const client = new MongoClient(db_string);
+
+let shoe;
+
+export const connectDB = async () => {
+  if (!shoe) {
+    try {
+      await client.connect();
+      const database = client.db("eCommerce_shoes");
+      shoe = database.collection("shoe");
+      liked_shoe = database.collection("liked_shoe");
+      console.log("Connected to database");
+    } catch (error) {
+      console.error("Failed to connect to database", error);
+    }
+  }
+};
+
+export { shoe, liked_shoe };
