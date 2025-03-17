740f617a7f43ca57b58d4c5f89ee6622a7532c22
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-03-17 14:36:21 -0300

    :sparkles: feat: Add /post url to add data for liked shoes

--- 10.webDeveloper/project/src/server.js
+++ 10.webDeveloper/project/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { connectDB, shoe, liked_shoe } from "../server/connect.js";
+import { ObjectId } from "mongodb";
 
 const app = express();
 const PORT = 5000;
@@ -56,6 +57,35 @@ app.get("/api/liked", async (req, res) => {
     await connectDB();
     const data = await liked_shoe.find({}).toArray();
     res.json(data);
+  } catch (error) {
+    console.error("Failed to fetch data:", error);
+    res.status(500).json({ erro: `Failed to fetch data` });
+  }
+});
+
+app.post("/api/liked", async (req, res) => {
+  try {
+    await connectDB();
+    const { created_at, shoe_id, username, is_liked = false } = req.body;
+    const isAlreadyCreated = await liked_shoe.findOne({
+      shoe_id: new ObjectId(shoe_id),
+    });
+
+    if (isAlreadyCreated) {
+      if (username === isAlreadyCreated["username"]) {
+        return res
+          .status(400)
+          .json({ message: "Shoe already mapped for this username" });
+      }
+    }
+
+    const data = await liked_shoe.insertOne({
+      created_at,
+      shoe_id: new ObjectId(shoe_id),
+      username,
+      is_liked: Boolean(is_liked),
+    });
+    res.status(200).json(data);
   } catch (error) {
     console.error("Failed to fetch data:", error);
     res.status(500).json({ erro: `Failed to fetch data` });
