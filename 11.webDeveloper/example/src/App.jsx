60de7bf4da1da26be312f14516d168ee7fe12625
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-03-18 18:07:15 -0300

    :books: docs: Put each to do on document and limit to 10 returning data

--- 11.webDeveloper/example/src/App.jsx
+++ 11.webDeveloper/example/src/App.jsx
@@ -2,10 +2,12 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import { listPosts } from "./services/post";
 import { listUsers } from "./services/user";
+import { listTodos } from "./services/todo";
 
 function App() {
   const [posts, setPosts] = useState([]);
   const [users, setUsers] = useState([]);
+  const [todos, setTodos] = useState([]);
 
   useEffect(() => {
     const response = async () => {
@@ -23,13 +25,28 @@ function App() {
     response();
   }, []);
 
+  useEffect(() => {
+    const response = async () => {
+      const { data } = await listTodos();
+      console.log(data);
+      setTodos(data);
+    };
+    response();
+  }, []);
+
   return (
     <>
-      {posts.map((post) => {
+      <h1>Posts:</h1>
+      {posts.slice(0, 10).map((post) => {
         return <p>{post.title}</p>;
       })}
-      {users.map((user) => {
+      <h1>User:</h1>
+      {users.slice(0, 10).map((user) => {
         return <p>{user.name}</p>;
+      })}
+      <h1>To do:</h1>
+      {todos.slice(0, 10).map((todo) => {
+        return <p>{todo.title}</p>;
       })}
     </>
   );
