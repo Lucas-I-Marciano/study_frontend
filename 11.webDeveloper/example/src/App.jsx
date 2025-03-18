ab1459ee8a2c9b920e8ee8f38355c935f27c2da6
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-03-18 17:53:10 -0300

    :books: docs: Put each title of query on document
    It will use listPosts Service

--- 11.webDeveloper/example/src/App.jsx
+++ 11.webDeveloper/example/src/App.jsx
@@ -1,7 +1,25 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import { listPosts } from "./services/post";
 
 function App() {
-  return <></>;
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const response = async () => {
+      const { data } = await listPosts();
+      console.log(data);
+      setPosts(data);
+    };
+    response();
+  }, []);
+  return (
+    <>
+      {posts.map((post) => {
+        return <p>{post.title}</p>;
+      })}
+    </>
+  );
 }
 
 export default App;
