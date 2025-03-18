e29701824ba69d9c5ccc6bf34736251897c118b4
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-03-17 21:08:27 -0300

    :recycle: refactor: Separate Header component from App

--- 10.webDeveloper/project/src/App.jsx
+++ 10.webDeveloper/project/src/App.jsx
@@ -1,14 +1,11 @@
 import "./App.css";
+import { Header } from "./components/Header/Header";
 import { ListCards } from "./components/ListCards/ListCards";
+
 function App() {
   return (
     <>
-      <header className="header">
-        <span className="header-logo">JAXONS</span>
-        <div className="header-highlight">
-          30% Discount on all products till 31st of december
-        </div>
-      </header>
+      <Header highlight="30% Discount on all products till 31st of december"></Header>
       <ListCards></ListCards>
     </>
   );
