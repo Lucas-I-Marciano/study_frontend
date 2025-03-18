c7b583e5cf4fa2a91ccaa6ec1251e1d01d14047d
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-03-18 18:02:43 -0300

    :books: docs: Put each name on document

--- 11.webDeveloper/example/src/App.jsx
+++ 11.webDeveloper/example/src/App.jsx
@@ -1,22 +1,35 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import { listPosts } from "./services/post";
+import { listUsers } from "./services/user";
 
 function App() {
   const [posts, setPosts] = useState([]);
+  const [users, setUsers] = useState([]);
 
   useEffect(() => {
     const response = async () => {
       const { data } = await listPosts();
-      console.log(data);
       setPosts(data);
     };
     response();
   }, []);
+
+  useEffect(() => {
+    const response = async () => {
+      const { data } = await listUsers();
+      setUsers(data);
+    };
+    response();
+  }, []);
+
   return (
     <>
       {posts.map((post) => {
         return <p>{post.title}</p>;
+      })}
+      {users.map((user) => {
+        return <p>{user.name}</p>;
       })}
     </>
   );
