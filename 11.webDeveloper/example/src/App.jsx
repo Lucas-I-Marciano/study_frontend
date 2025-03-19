f6fdd6c96e50ae6cb00f2f40033c0551218f8a95
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-03-19 13:41:43 -0300

    :books: docs: Create first route

--- 11.webDeveloper/example/src/App.jsx
+++ 11.webDeveloper/example/src/App.jsx
@@ -4,12 +4,27 @@ import { Post } from "./components/Post";
 import { Users } from "./components/Users";
 import { Todos } from "./components/Todos";
 
+import { createBrowserRouter, RouterProvider } from "react-router";
+
+let router = createBrowserRouter([
+  {
+    path: "/",
+    Component: () => {
+      return (
+        <>
+          <Post numPosts={3} />
+          <Users numUsers={3} />
+          <Todos numTodos={3} />
+        </>
+      );
+    },
+  },
+]);
+
 function App() {
   return (
     <>
-      <Post numPosts={4}></Post>
-      <Users numUsers={5}></Users>
-      <Todos numTodos={6}></Todos>
+      <RouterProvider router={router}></RouterProvider>
     </>
   );
 }
