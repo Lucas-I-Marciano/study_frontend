dcb7a03a49f4b3849651dc5467e8572f97c1d7f3
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-03-19 13:24:28 -0300

    :recycle: refactor: Refactor post query to its component

--- 11.webDeveloper/example/src/App.jsx
+++ 11.webDeveloper/example/src/App.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import { listPosts } from "./services/post";
 import { listUsers } from "./services/user";
 import { listTodos } from "./services/todo";
 
@@ -9,17 +8,8 @@ import { Users } from "./components/Users";
 import { Todos } from "./components/Todos";
 
 function App() {
-  const [posts, setPosts] = useState([]);
   const [users, setUsers] = useState([]);
   const [todos, setTodos] = useState([]);
-
-  useEffect(() => {
-    const response = async () => {
-      const { data } = await listPosts();
-      setPosts(data);
-    };
-    response();
-  }, []);
 
   useEffect(() => {
     const response = async () => {
@@ -40,7 +30,7 @@ function App() {
 
   return (
     <>
-      <Post posts={posts} numPosts={4}></Post>
+      <Post numPosts={4}></Post>
       <Users users={users} numUsers={4}></Users>
       <Todos todos={todos} numTodos={4}></Todos>
     </>
--- 11.webDeveloper/example/src/components/Post.jsx
+++ 11.webDeveloper/example/src/components/Post.jsx
@@ -1,4 +1,16 @@
-export const Post = ({ posts, numPosts }) => {
+import { useEffect, useState } from "react";
+import { listPosts } from "../services/post";
+
+export const Post = ({ numPosts }) => {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    const response = async () => {
+      const { data } = await listPosts();
+      setPosts(data);
+    };
+    response();
+  }, []);
+
   return (
     <div className="post">
       <h1>Posts:</h1>
