3d06ca72779de434dd13782448d7555c0388ade6
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-03-19 14:44:45 -0300

    :books: docs: Create function to list specific user data

--- 11.webDeveloper/example/src/services/user.js
+++ 11.webDeveloper/example/src/services/user.js
@@ -3,3 +3,7 @@ import instance from "../providers/client";
 export const listUsers = async () => {
   return await instance.get("users");
 };
+
+export const listUser = async (id) => {
+  return await instance.get(`users/${id}`);
+};
