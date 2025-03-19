661de80ff2748080ae8583678845c80d42bbc1ed
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-03-19 18:54:43 -0300

    :books: study: Add useLocation to get use of query/search params on Users url

--- 11.webDeveloper/example/src/components/Users.jsx
+++ 11.webDeveloper/example/src/components/Users.jsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
 import { listUser, listUsers } from "../services/user";
-import { NavLink, useParams } from "react-router";
+import { NavLink, useLocation, useParams } from "react-router";
 
-export const Users = ({ numUsers }) => {
+export const Users = () => {
   const [users, setUsers] = useState([]);
   useEffect(() => {
     const response = async () => {
@@ -11,12 +11,21 @@ export const Users = ({ numUsers }) => {
     };
     response();
   }, []);
+
+  const { search } = useLocation();
+  const urlSearchParam = new URLSearchParams(search);
+  const max = parseInt(urlSearchParam.get("max"));
+  const offset = parseInt(urlSearchParam.get("offset"));
+
+  const maxElement = max ? max : 10;
+  const offsetElement = offset ? offset : 0;
+
   return (
     <div className="users">
       <NavLink to="/users">
         <h1>Users:</h1>
       </NavLink>
-      {users.slice(0, numUsers).map((user) => {
+      {users.slice(offsetElement, offsetElement + maxElement).map((user) => {
         return (
           <NavLink to={`/users/${user.id}`}>
             <p>{user.name}</p>
