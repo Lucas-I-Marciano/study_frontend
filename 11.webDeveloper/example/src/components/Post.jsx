3960f863b93298a29fd13a03ba39c80ab9ce0fdc
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-03-19 18:51:22 -0300

    :books: study: Add useLocation to get use of query/search params

--- 11.webDeveloper/example/src/components/Post.jsx
+++ 11.webDeveloper/example/src/components/Post.jsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
 import { listPosts } from "../services/post";
-import { NavLink } from "react-router";
+import { NavLink, useLocation } from "react-router";
 
-export const Post = ({ numPosts }) => {
+export const Post = () => {
   const [posts, setPosts] = useState([]);
   useEffect(() => {
     const response = async () => {
@@ -12,12 +12,20 @@ export const Post = ({ numPosts }) => {
     response();
   }, []);
 
+  const { search } = useLocation();
+  const urlSearchParam = new URLSearchParams(search);
+  const max = parseInt(urlSearchParam.get("max"));
+  const offset = parseInt(urlSearchParam.get("offset"));
+
+  const maxElement = max ? max : 10;
+  const offsetElement = offset ? offset : 0;
+
   return (
     <div className="post">
       <NavLink to="/posts">
         <h1>Posts:</h1>
       </NavLink>
-      {posts.slice(0, numPosts).map((post) => {
+      {posts.slice(offsetElement, offsetElement + maxElement).map((post) => {
         return <p>{post.title}</p>;
       })}
     </div>
