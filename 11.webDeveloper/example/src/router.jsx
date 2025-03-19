0ba3c60a17b85a828a0ee3fa136f164216b00b4c
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-03-19 14:00:12 -0300

    :books: docs: Create specific route for users, posts and todos

--- 11.webDeveloper/example/src/router.jsx
+++ 11.webDeveloper/example/src/router.jsx
@@ -1,4 +1,7 @@
 import DefaultPath from "./components/DefaultPath";
+import { Post } from "./components/Post";
+import { Todos } from "./components/Todos";
+import { Users } from "./components/Users";
 
 export default [
   {
@@ -9,6 +12,24 @@ export default [
           <DefaultPath />
         </>
       );
+    },
+  },
+  {
+    path: "/users",
+    Component: () => {
+      return <Users numUsers={5} />;
+    },
+  },
+  {
+    path: "/posts",
+    Component: () => {
+      return <Post numPosts={5} />;
+    },
+  },
+  {
+    path: "/todos",
+    Component: () => {
+      return <Todos numTodos={5} />;
     },
   },
 ];
