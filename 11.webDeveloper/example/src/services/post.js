b6fb23b46e414517790cd209d9a4cb6fbdfefce4
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-03-19 19:31:47 -0300

    :books: study: Add service to list post by user

--- 11.webDeveloper/example/src/services/post.js
+++ 11.webDeveloper/example/src/services/post.js
@@ -3,3 +3,7 @@ import instance from "../providers/client";
 export const listPosts = async () => {
   return await instance.get("/posts");
 };
+
+export const listPostsUser = async (filter) => {
+  return await instance.get(`/posts${filter || ""}`);
+};
