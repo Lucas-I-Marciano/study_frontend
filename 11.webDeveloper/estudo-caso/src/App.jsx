06f1e7c83fd046c0d5a21bdc687c7747d792460a
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-03-20 19:54:23 -0300

    :bug: fix: Put Navbar inside RouteProvider to fix router context error
    Uncaught Error: useLocation() may be used only in the context of a <Router> component.

--- 11.webDeveloper/estudo-caso/src/App.jsx
+++ 11.webDeveloper/estudo-caso/src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import { Navbar } from "./components/Navbar";
-import { RouterProvider } from "react-router";
+import { RouterProvider, Outlet } from "react-router";
 import router from "./routes/router";
 
 function App() {
   return (
     <>
-      <RouterProvider router={router} />
+      <RouterProvider router={router}>
+        <Navbar />
+      </RouterProvider>
     </>
   );
 }
