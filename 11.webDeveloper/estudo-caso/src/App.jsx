07b685a80ead14f9adf5707d12e1c3c3c976b5fe
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-03-20 18:00:29 -0300

    :books: study: Add router to App

--- 11.webDeveloper/estudo-caso/src/App.jsx
+++ 11.webDeveloper/estudo-caso/src/App.jsx
@@ -1,18 +1,12 @@
 import "./App.css";
-import { Login } from "./components/Login";
-import { Home } from "./components/Home";
 import { Navbar } from "./components/Navbar";
-import { ProtectPage } from "./components/ProtectPage";
-import { PublicPage } from "./components/PublicPage";
+import { RouterProvider } from "react-router";
+import router from "./routes/router";
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Home />
-      <ProtectPage />
-      <PublicPage />
-      <Login />
+      <RouterProvider router={router} />
     </>
   );
 }
