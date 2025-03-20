c4a71c811185963d0fadfa17fd7478855fe9f46e
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-03-20 18:01:11 -0300

    :books: study: Add navigation on my Navbar component

--- 11.webDeveloper/estudo-caso/src/components/Navbar.jsx
+++ 11.webDeveloper/estudo-caso/src/components/Navbar.jsx
@@ -1,7 +1,17 @@
+import { NavLink } from "react-router";
+
 export const Navbar = () => {
   return (
     <div>
       <h1>Bem vindo</h1>
+      <ul>
+        <li>
+          <NavLink to="/public">Public</NavLink>
+        </li>
+        <li>
+          <NavLink to="/private">Private</NavLink>
+        </li>
+      </ul>
     </div>
   );
 };
