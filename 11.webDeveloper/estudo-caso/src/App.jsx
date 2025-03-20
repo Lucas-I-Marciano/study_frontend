919291b21ac46c4bbdce70c66a668b3ed0c95e0f
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-03-20 17:38:44 -0300

    :books: study: Add all created components on App

--- 11.webDeveloper/estudo-caso/src/App.jsx
+++ 11.webDeveloper/estudo-caso/src/App.jsx
@@ -1,7 +1,20 @@
 import "./App.css";
+import { Login } from "./components/Login";
+import { Home } from "./components/Home";
+import { Navbar } from "./components/Navbar";
+import { ProtectPage } from "./components/ProtectPage";
+import { PublicPage } from "./components/PublicPage";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Navbar />
+      <Home />
+      <ProtectPage />
+      <PublicPage />
+      <Login />
+    </>
+  );
 }
 
 export default App;
