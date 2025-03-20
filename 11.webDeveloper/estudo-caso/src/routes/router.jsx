42423a1523a979255e5590b5feba767500dae0b5
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-03-20 19:52:47 -0300

    :books: study: Add Layout on root path to extend to its children

--- 11.webDeveloper/estudo-caso/src/routes/router.jsx
+++ 11.webDeveloper/estudo-caso/src/routes/router.jsx
@@ -3,33 +3,30 @@ import { Home } from "../components/Home";
 import { Login } from "../components/Login";
 import { ProtectPage } from "../components/ProtectPage";
 import { PublicPage } from "../components/PublicPage";
-import { Navbar } from "../components/Navbar";
-
-const returnElementAndNav = (element) => {
-  return (
-    <>
-      <Navbar />
-      {element}
-    </>
-  );
-};
+import { Layout } from "../components/Layout";
 
 const routes = [
   {
     path: "/",
-    element: returnElementAndNav(<Home />),
-  },
-  {
-    path: "/login",
-    element: returnElementAndNav(<Login />),
-  },
-  {
-    path: "/public",
-    element: returnElementAndNav(<PublicPage />),
-  },
-  {
-    path: "/private",
-    element: returnElementAndNav(<ProtectPage />),
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/private",
+        element: <ProtectPage />,
+      },
+      {
+        path: "/public",
+        element: <PublicPage />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+    ],
   },
 ];
 
