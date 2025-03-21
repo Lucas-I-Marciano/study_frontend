78db55c18b48a266fe87d7b4a1ad72cbbebd9a98
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-03-21 20:23:11 -0300

    :books: study: Use getGeneral from service to 'GET /' on backend

--- 11.webDeveloper/estudo-caso/src/components/PublicPage.jsx
+++ 11.webDeveloper/estudo-caso/src/components/PublicPage.jsx
@@ -1,4 +1,15 @@
+import { useEffect } from "react";
+import { getGeneral } from "../services/general";
+
 export const PublicPage = () => {
+  useEffect(() => {
+    const toQuery = async () => {
+      const response = await getGeneral();
+      console.log("response", response);
+    };
+    toQuery();
+  });
+
   return (
     <div>
       <h1>PublicPage</h1>
