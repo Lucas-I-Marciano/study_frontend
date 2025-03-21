8a9d7fe65d3da08cb5c0c45a5cb41ac441bf9531
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-03-20 22:23:30 -0300

    :books: study: Validate authentication from <form> data

--- 11.webDeveloper/estudo-caso/src/components/Login.jsx
+++ 11.webDeveloper/estudo-caso/src/components/Login.jsx
@@ -1,7 +1,31 @@
+import { useEffect, useState } from "react";
+import { authService } from "../services/auth";
+
 export const Login = () => {
+  const [email, setEmail] = useState(null);
+
+  const submitEvent = (event) => {
+    event.preventDefault();
+    var data = new FormData(event.target);
+    for (var [key, value] of data) {
+      if (key === "email") {
+        setEmail(value);
+      }
+    }
+  };
+  useEffect(() => {
+    const toQuery = async () => {
+      const response = await authService({ email: email });
+      const toStorage = response.data?.token ? response.data.token : "";
+      localStorage.setItem("userToken", toStorage);
+    };
+    if (email && email !== null) {
+      toQuery();
+    }
+  }, [email]);
   return (
     <div>
-      <form>
+      <form onSubmit={submitEvent}>
         <label>
           Email:
           <input name="email" type="text"></input>
