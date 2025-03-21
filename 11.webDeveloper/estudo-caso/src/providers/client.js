85737337d9674cc6d4a0628c01a41ecc06c6d8ea
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-03-21 20:22:02 -0300

    :books: study: Add interceptor (middleware) on axios instance to put userToken on headers

--- 11.webDeveloper/estudo-caso/src/providers/client.js
+++ 11.webDeveloper/estudo-caso/src/providers/client.js
@@ -4,4 +4,16 @@ const instance = axios.create({
   baseURL: "http://localhost:3000",
 });
 
+instance.interceptors.request.use((config) => {
+  const userToken = localStorage.getItem("userToken");
+
+  return {
+    ...config,
+    headers: {
+      ...config.headers,
+      Authorization: userToken ? `Bearer ${userToken}` : "",
+    },
+  };
+});
+
 export default instance;
