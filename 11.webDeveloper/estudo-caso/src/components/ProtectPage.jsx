5e0cec667e92738847e2a0ec89826bc5f1ccc641
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-03-22 10:16:38 -0300

    :books: study: Prevent access without correct 'userToken' token

--- 11.webDeveloper/estudo-caso/src/components/ProtectPage.jsx
+++ 11.webDeveloper/estudo-caso/src/components/ProtectPage.jsx
@@ -1,7 +1,25 @@
+import { jwtDecode } from "jwt-decode";
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
+
 export const ProtectPage = () => {
-  return (
-    <div>
-      <h1>ProtectPage</h1>
-    </div>
-  );
+  const userToken = localStorage.getItem("userToken");
+  const navigate = useNavigate();
+
+  try {
+    if (userToken | (userToken == null)) {
+      throw Error;
+    }
+    const decoded = jwtDecode(userToken);
+    console.log(decoded);
+    return (
+      <div>
+        <h1>ProtectPage</h1>
+      </div>
+    );
+  } catch {
+    useEffect(() => {
+      navigate("/login");
+    });
+  }
 };
