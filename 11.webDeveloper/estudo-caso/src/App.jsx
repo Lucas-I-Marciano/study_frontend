953b85e6b944ff40f1689fcf95185939a2ac9c29
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-03-23 21:26:42 -0300

    :books: study: Import and use AuthProvider to get user info globally

--- 11.webDeveloper/estudo-caso/src/App.jsx
+++ 11.webDeveloper/estudo-caso/src/App.jsx
@@ -2,13 +2,16 @@ import "./App.css";
 import { Navbar } from "./components/Navbar";
 import { RouterProvider, Outlet } from "react-router";
 import router from "./routes/router";
+import { AuthProvider } from "./context/AuthContext";
 
 function App() {
   return (
     <>
-      <RouterProvider router={router}>
-        <Navbar />
-      </RouterProvider>
+      <AuthProvider>
+        <RouterProvider router={router}>
+          <Navbar />
+        </RouterProvider>
+      </AuthProvider>
     </>
   );
 }
