43ffcaece8b63394bc3f402b0e0ed8ddca0ce435
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-04-01 17:56:15 -0300

    feat: Add useState hook to manage global user state

--- 11.webDeveloper/estudo-caso/src/context/AuthContext.jsx
+++ 11.webDeveloper/estudo-caso/src/context/AuthContext.jsx
@@ -1,8 +1,11 @@
+import { useState } from "react";
 import { AuthContext } from "./AuthC";
 
 export const AuthProvider = (props) => {
+  const [user, setUser] = useState("Default user");
+
   return (
-    <AuthContext.Provider value={{ user: "Lucas" }}>
+    <AuthContext.Provider value={{ user, setUser }}>
       {props.children}
     </AuthContext.Provider>
   );
