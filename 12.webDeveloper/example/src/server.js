f7ca532a03dc78194be2b8923bc21e01a81bf1ad
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-04-02 18:35:50 -0300

    chore: Enable CORS and add base test route

--- 12.webDeveloper/example/src/server.js
+++ 12.webDeveloper/example/src/server.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
 import bodyParser from 'body-parser';
+import cors from "cors"
 
 const app = express();
 const PORT = 3000;
 const SECRET_KEY = 'your_secret_key';
 
 app.use(bodyParser.json());
+app.use(cors())
 
 // Fake database
 const fake_database = [
@@ -16,44 +18,50 @@ const fake_database = [
 
 // Login route
 app.post('/login', (req, res) => {
-  const { email, password } = req.body;
+    console.log("Rota de Login");
+    
+    const { email, password } = req.body;
 
-  // Find user in the fake database
-  const user = fake_database.find(u => u.email === email && u.password === password);
-
+    // Find user in the fake database
+    const user = fake_database.find(u => u.email === email && u.password === password);
+    
   if (user) {
-    // Generate JWT token
-    const token = jwt.sign({ username: user.username, email: user.email }, SECRET_KEY, { expiresIn: '1h' });
+      // Generate JWT token
+      const token = jwt.sign({ username: user.username, email: user.email }, SECRET_KEY, { expiresIn: '1h' });
     res.json({ token });
-  } else {
+} else {
     res.status(401).json({ message: 'Invalid email or password' });
   }
 });
 
 // Middleware to verify token
 const verifyToken = (req, res, next) => {
-  const token = req.headers['authorization'];
-
-  if (!token) {
-    return res.status(403).json({ message: 'No token provided' });
-  }
-
-  jwt.verify(token, SECRET_KEY, (err, decoded) => {
-    if (err) {
-      return res.status(500).json({ message: 'Failed to authenticate token' });
+    const token = req.headers['authorization'];
+    
+    if (!token) {
+        return res.status(403).json({ message: 'No token provided' });
     }
-
-    req.user = decoded;
-    next();
+    
+    jwt.verify(token, SECRET_KEY, (err, decoded) => {
+        if (err) {
+            return res.status(500).json({ message: 'Failed to authenticate token' });
+        }
+        
+        req.user = decoded;
+        next();
   });
 };
 
 // Route to get decoded token
 app.get('/me', verifyToken, (req, res) => {
-  res.json({ email: req.user.email, username: req.user.username });
+    console.log("Rota de Verificação");
+    res.json({ email: req.user.email, username: req.user.username });
 });
 
-app.get("/", (req, res)=>{res.json({message: "Ok!"})})
+app.get("/", (req, res)=>{
+    console.log("Rota de Teste");
+    res.json({message: "Ok!"})
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
