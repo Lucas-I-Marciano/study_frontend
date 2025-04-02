6a59612d54d5852750548d0204997a6f13610ae6
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-04-02 19:00:49 -0300

    Feat(app): Mock login request and trigger useEffect to fetch user data on token generation

--- 12.webDeveloper/example/src/App.jsx
+++ 12.webDeveloper/example/src/App.jsx
@@ -1,23 +1,43 @@
 import { useEffect } from 'react'
 import './App.css'
 
-import { root } from './services/auth'
+import { login, getMe } from './services/auth'
 import { useState } from 'react'
 
 function App() {
-  const [testRoot, setTestRoot] = useState("")
+  const [token, setToken] = useState("")
+  const [user, setUser] = useState({})
 
   useEffect(()=>{
     const toQuery = async ()=>{
-      const response = await root()
-      setTestRoot(response.message)
+      const response = await login(
+        {
+          "email": "user2@example.com",
+          "password": "password2"
+        }
+      )
+      setToken(response.token)
     }
     toQuery()
   },[])
+  
+  useEffect(()=>{
+    const toQuery = async ()=>{
+      try {
+        const response = await getMe(token)
+        setUser(response)
+        
+      } catch (error) {
+        console.error("ERRO: ", error)
+      }
+    }
+    toQuery()
+  },[token])
 
   return (
     <>
-      <p className='bg-red-500'>{testRoot}</p>
+      <p className='bg-blue-100 p-2 w-100 rounded-xl m-2'>E-mail: {user.email}</p>
+      <p className='bg-blue-100 p-2 w-100 rounded-xl m-2'>Username: {user.username}</p>
     </>
   )
 }
