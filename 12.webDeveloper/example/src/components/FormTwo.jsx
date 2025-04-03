460b93b09faa083a8858b17984449351aa041d2b
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-04-03 18:19:40 -0300

    Feat: Add yup validation schema and update error messages to be dynamic

--- 12.webDeveloper/example/src/components/FormTwo.jsx
+++ 12.webDeveloper/example/src/components/FormTwo.jsx
@@ -1,35 +1,44 @@
 import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup"
 import { login } from "../services/auth";
+import * as yup from "yup"
 
-export const FormTwo = ()=>{
-    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+const validationSchema = yup.object({
+  email: yup.string().email("Invalid email").required("You must register with an valid email"),
+  password: yup.string().required("You must enter a password"),
+}).required()
+
+export const FormTwo = () => {
+  const { register, handleSubmit, watch, formState: { errors } } = useForm({ resolver: yupResolver(validationSchema) });
 
 
   const onSubmit = async (data) => {
-      const response = await login(
-        {
-          "email": data.email,
-          "password": data.password
-        }
-      )
-      console.log(response)
-    }
-  
+    const response = await login(
+      {
+        "email": data.email,
+        "password": data.password
+      }
+    )
+    console.log(response)
+  }
+  console.log(errors);
 
-  console.log("Email changed: ", watch("email")) // watch input value by passing the name of it
+
+  // console.log("Email changed: ", watch("email")) // watch input value by passing the name of it
 
   return (
     /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
     <form onSubmit={handleSubmit(onSubmit)} className='bg-blue-100 p-10'>
       {/* register your input into the hook by invoking the "register" function */}
       <input className='border-1 border-solid m-2' placeholder='your_email@email.com' {...register("email", { required: true })} />
-      {errors.email && <span>This field is required</span>}
-      
+      {errors.email && <span>{errors.email.message}</span>}
+      <br></br>
       {/* include validation with required or other standard HTML validation rules */}
       <input type='password' className='border-1 border-solid m-2' {...register("password", { required: true })} />
       {/* errors will return when field validation fails  */}
-      {errors.password && <span>This field is required</span>}
-      
+      {errors.password && <span>{errors.password.message}</span>}
+
+      <br></br>
       <input className='bg-gray-500 text-white p-1 m-2' type="submit" />
 
       { /*console.log(errors) /* {exampleRequired : {message:"", type:"required"}} --> exampleRequired is field name*/}
