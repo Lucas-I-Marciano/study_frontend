32a301836f779f7a9b23b3b39524c5376c12ec74
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-04-03 12:35:40 -0300

    Feat(app): Add basic example of react-hook-form to App component

--- 12.webDeveloper/example/src/App.jsx
+++ 12.webDeveloper/example/src/App.jsx
@@ -4,6 +4,8 @@ import './App.css'
 import { login, getMe } from './services/auth'
 import { useState } from 'react'
 
+import { useForm } from "react-hook-form";
+
 function App() {
   const [token, setToken] = useState("")
   const [user, setUser] = useState({})
@@ -40,6 +42,28 @@ function App() {
       <p className='bg-blue-100 p-2 w-100 rounded-xl m-2'>Username: {user.username}</p>
     </>
   )
+}
+
+export const App2 = ()=> {
+  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+  const onSubmit = data => console.log(data);
+
+  console.log(watch("example")); // watch input value by passing the name of it
+
+  return (
+    /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
+    <form onSubmit={handleSubmit(onSubmit)} className='bg-blue-100 p-10'>
+      {/* register your input into the hook by invoking the "register" function */}
+      <input className='border-1 border-solid m-2' defaultValue="test" {...register("example")} />
+      
+      {/* include validation with required or other standard HTML validation rules */}
+      <input className='border-1 border-solid m-2' {...register("exampleRequired", { required: true })} />
+      {/* errors will return when field validation fails  */}
+      {errors.exampleRequired && <span>This field is required</span>}
+      
+      <input className='bg-gray-500 text-white p-1 m-2' type="submit" />
+    </form>
+  );
 }
 
 export default App
--- 12.webDeveloper/example/src/main.jsx
+++ 12.webDeveloper/example/src/main.jsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import App, { App2 } from './App.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <App2 />
   </StrictMode>,
 )
