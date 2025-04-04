7c92253076888dd2111018b457d746fde92c4840
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-04-04 20:38:09 -0300

    feat: Add initial routes for car creation and editing

--- 12.webDeveloper/estudo-caso/src/App.jsx
+++ 12.webDeveloper/estudo-caso/src/App.jsx
@@ -1,11 +1,15 @@
 import { Form } from "./components/Form"
+import { Routes, Route } from "react-router";
 import "./App.css"
 
 function App() {
 
   return (
     <>
-      <Form />
+      <Routes>
+        <Route path="/car/create" element={<Form />} />
+        <Route path="/car/edit/:id" element={<Form />} />
+      </Routes>
     </>
   )
 }
