7f1c7146c3d8e8a23a0fdcf3a6a8c05c006a7a79
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-04-03 22:14:51 -0300

    Chore: Update rendered component to FormExercise in App.jsx

--- 12.webDeveloper/example/src/App.jsx
+++ 12.webDeveloper/example/src/App.jsx
@@ -2,9 +2,11 @@ import './App.css'
 
 import { FormOne } from './components/FormOne';
 import { FormTwo } from './components/FormTwo';
+import { FormExercise } from './components/FormExercise';
+
 
 function App() {
-  return <FormTwo/>
+  return <FormExercise />
 }
 
 export default App
