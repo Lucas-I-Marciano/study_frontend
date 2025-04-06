055f05fd85ceca54fb6086d68118cd19d0b047a3
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-04-06 19:25:52 -0300

    feat: Add description field to global context for application use

--- 12.webDeveloper/project/src/context/bd.jsx
+++ 12.webDeveloper/project/src/context/bd.jsx
@@ -7,26 +7,31 @@ const db = {
         price: 6,
         currency: "BRL",
         measure: "KG",
+        description: "A banana is an elongated, edible fruit – botanically a berry[1] – produced by several kinds of large treelike herbaceous flowering plants in the genus Musa. In some countries, cooking bananas are called plantains, distinguishing them from dessert bananas",
     },
     apple: {
         price: 3,
         currency: "BRL",
         measure: "KG",
+        description: "An apple is a round, edible fruit produced by an apple tree (Malus spp.). Fruit trees of the orchard or domestic apple (Malus domestica), the most widely grown in the genus, are cultivated worldwide",
     },
     orange: {
         price: 5.5,
         currency: "BRL",
         measure: "KG",
+        description: "The orange, also called sweet orange to distinguish it from the bitter orange (Citrus × aurantium), is the fruit of a tree in the family Rutaceae. Botanically, this is the hybrid Citrus × sinensis, between the pomelo (Citrus maxima) and the mandarin orange (Citrus reticulata)",
     },
     avocado: {
         price: 5,
         currency: "BRL",
         measure: "UN",
+        description: "The avocado, alligator pear or avocado pear (Persea americana) is an evergreen tree in the laurel family (Lauraceae). It is native to the Americas and was first domesticated in Mesoamerica more than 5,000 years ago",
     },
     mango: {
         price: 6,
         currency: "BRL",
         measure: "UN",
+        description: "Mango is native to India and Southeast Asia, and people have cultivated it for over 4,000 years.Hundreds of types of mango exist, each with its own characteristic taste, shape, size, and color",
     },
 }
 
