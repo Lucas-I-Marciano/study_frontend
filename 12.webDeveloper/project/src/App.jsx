726fae2200fe112b0a71fa8541fff979dcbbaeaa
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-04-06 16:18:57 -0300

    feat: Import Cart, Home components and BdContextProvider into App.jsx

--- 12.webDeveloper/project/src/App.jsx
+++ 12.webDeveloper/project/src/App.jsx
@@ -1,22 +1,25 @@
 import './App.css'
 import { Routes, Route } from "react-router";
 
-const Home = () => {
-  return <h1>Home Page</h1>
-}
+import { BdContextProvider } from "./context/bd"
+import { Cart } from "./components/Cart"
+import { Home } from "./components/Home"
 
-const Cart = () => {
-  return <h1>Cart Page</h1>
-}
+
+
+
+
 
 function App() {
 
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/cart" element={<Cart />}></Route>
-      </Routes>
+      <BdContextProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </BdContextProvider>
     </>
   )
 }
