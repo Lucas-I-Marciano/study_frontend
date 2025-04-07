79d53280a13f2a058e04f1c7f8fc80033c927ef3
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-04-07 12:47:45 -0300

    style: Style Product component with Tailwind CSS and add measure prop

--- 12.webDeveloper/project/src/components/Product.jsx
+++ 12.webDeveloper/project/src/components/Product.jsx
@@ -1,17 +1,24 @@
-export const Product = ({ currency, price, title, description, image }) => {
+export const Product = ({ currency, price, title, description, image, measure }) => {
     return (
         <>
-            <div>
-                <img src={image} />
-            </div>
-            <div>
-                <p>{title}</p>
-                <p>{description}</p>
+            <div className="w-70 flex flex-col items-center p-5 shadow-2xl m-4 rounded-2xl">
+                <div>
+                    <img className="w-35 pb-1" src={image} />
+                </div>
+                <div>
+                    <p className="text-2xl pb-1">{title}</p>
+                    <p className="text-xs pb-4 text-justify">{description}</p>
 
-            </div>
-            <div>
-                <span>{currency} {price}</span>
-                <button>Add to Cart</button>
+                </div>
+                <div className="flex justify-between w-full">
+                    <span className="font-bold text-lg text-gray-700">{currency} {price} /{measure}</span>
+                    <button className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-3 py-1.5 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 flex items-center">
+                        Add to Cart
+                        <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                        </svg>
+                    </button>
+                </div>
             </div>
         </>
     )
