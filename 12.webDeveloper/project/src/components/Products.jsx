3be61ee295c796c0ca88a4884bb00f3cb78a1e72
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-04-07 20:47:58 -0300

    feat: Navigate to cart page on button click

--- 12.webDeveloper/project/src/components/Products.jsx
+++ 12.webDeveloper/project/src/components/Products.jsx
@@ -1,7 +1,7 @@
 import { useBdContext } from "../context/bd"
 import { Product } from "./Product"
 import { allImages } from "../assets"
-import { useState } from "react"
+import { useNavigate } from "react-router";
 
 
 
@@ -15,6 +15,7 @@ export const Products = () => {
         setTotal(finalPrice)
     }
 
+    let navigate = useNavigate();
 
     return (
         <div className="flex flex-col items-center">
@@ -29,7 +30,12 @@ export const Products = () => {
                         measure={mockDb[element].measure} />
                 })}
             </div>
-            <button onClick={() => { closeCart(cart) }} className="w-200 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Go to cart</button>
+            <button
+                onClick={() => {
+                    closeCart(cart)
+                    navigate('/cart')
+                }}
+                className="w-200 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Go to cart</button>
         </div>
     )
 }
