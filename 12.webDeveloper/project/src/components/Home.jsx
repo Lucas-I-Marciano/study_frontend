eff8034f918a03bf482e9cf3ebf730d08be63c1b
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-04-07 12:50:28 -0300

    refactor: Create Products component to manage rendering of Product components

--- 12.webDeveloper/project/src/components/Home.jsx
+++ 12.webDeveloper/project/src/components/Home.jsx
@@ -1,24 +1,8 @@
-import { useBdContext } from "../context/bd"
-import { Product } from "./Product"
-import { allImages } from "../assets"
+import { Products } from "./Products"
 
 
 export const Home = () => {
-    const handleBd = useBdContext()
-    console.log(handleBd);
-
-    const allProducts = Object.keys(handleBd.mockDb)
-
-
     return (<>
-        {allProducts.map(element => {
-            return <Product
-                currency={handleBd.mockDb[element].currency}
-                price={handleBd.mockDb[element].price}
-                title={element}
-                description={handleBd.mockDb[element].description}
-                image={allImages[element]}
-                measure={handleBd.mockDb[element].measure} />
-        })}
+        <Products />
     </>)
 }
---
+++ 12.webDeveloper/project/src/components/Products.jsx
@@ -0,0 +1,24 @@
+import { useBdContext } from "../context/bd"
+import { Product } from "./Product"
+import { allImages } from "../assets"
+
+
+
+export const Products = () => {
+    const handleBd = useBdContext()
+    const allProducts = Object.keys(handleBd.mockDb)
+
+    return (
+        <div>
+            {allProducts.map(element => {
+                return <Product
+                    currency={handleBd.mockDb[element].currency}
+                    price={handleBd.mockDb[element].price}
+                    title={element}
+                    description={handleBd.mockDb[element].description}
+                    image={allImages[element]}
+                    measure={handleBd.mockDb[element].measure} />
+            })}
+        </div>
+    )
+}
