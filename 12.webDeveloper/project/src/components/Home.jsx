51878d130b1fda7e19ecf7469645dff0c3eee577
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-04-07 20:46:17 -0300

    style: Add title and style rendered components in Home.tsx

--- 12.webDeveloper/project/src/components/Home.jsx
+++ 12.webDeveloper/project/src/components/Home.jsx
@@ -2,7 +2,8 @@ import { Products } from "./Products"
 
 
 export const Home = () => {
-    return (<>
+    return (<div className="flex flex-col items-center">
+        <h1 className="text-3xl text-blue-700 font-serif text-shadow-4  xs">Marciano's Store!</h1>
         <Products />
-    </>)
+    </div>)
 }
