bfe2a5ecc10b50c99111aa5496bcd34e45c7938c
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-04-07 20:37:12 -0300

    feat: Implement logic in Products component to update global total based on cart items

--- 12.webDeveloper/project/src/components/Products.jsx
+++ 12.webDeveloper/project/src/components/Products.jsx
@@ -1,24 +1,35 @@
 import { useBdContext } from "../context/bd"
 import { Product } from "./Product"
 import { allImages } from "../assets"
+import { useState } from "react"
 
 
 
 export const Products = () => {
-    const handleBd = useBdContext()
-    const allProducts = Object.keys(handleBd.mockDb)
+    const { mockDb, cart, total, setTotal } = useBdContext()
+    const allProducts = Object.keys(mockDb)
+
+    const closeCart = (cartObject) => {
+        const entries = Object.entries(cartObject)
+        const finalPrice = entries.reduce((acc, [_, itsObject]) => { return acc + itsObject.price * itsObject.quantity }, 0)
+        setTotal(finalPrice)
+    }
+
 
     return (
-        <div className="flex flex-wrap">
-            {allProducts.map(element => {
-                return <Product
-                    currency={handleBd.mockDb[element].currency}
-                    price={handleBd.mockDb[element].price}
-                    title={element}
-                    description={handleBd.mockDb[element].description}
-                    image={allImages[element]}
-                    measure={handleBd.mockDb[element].measure} />
-            })}
+        <div className="flex flex-col items-center">
+            <div className="flex flex-wrap justify-between">
+                {allProducts.map(element => {
+                    return <Product
+                        currency={mockDb[element].currency}
+                        price={mockDb[element].price}
+                        title={element}
+                        description={mockDb[element].description}
+                        image={allImages[element]}
+                        measure={mockDb[element].measure} />
+                })}
+            </div>
+            <button onClick={() => { closeCart(cart) }} className="w-200 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Go to cart</button>
         </div>
     )
 }
