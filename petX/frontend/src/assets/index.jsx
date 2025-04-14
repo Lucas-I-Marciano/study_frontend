7b4216d6b3e647140c43687f0f9b62996111ec88
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-04-13 23:05:57 -0300

    feat: Create index.jsx in assets folder to import and export images for application

---
+++ petX/frontend/src/assets/index.jsx
@@ -0,0 +1,10 @@
+import login_paw from "./login_paw.png"
+import login_back from "./login_background.jpg"
+
+const images = {
+    login_paw,
+    login_back,
+    login_back_url: "https://s3-alpha-sig.figma.com/img/c1af/9e5e/803ab5229112158c076d2b996f8e4e8d?Expires=1745798400&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=VomQPGQaC2PJ1HIApHqUfWz9kDBFKODyP6Mv4jU8LBurf8E1qJK1n0Orxlq0gC1Eih9ManywokdtJ-zQWMmV-XDixv9Qjdz~0u8iYa0Ya1MAlU~t3uQT8TqYP9LjbFYfBdgbdRrYNeoI27jqBkGxZXohRm00XyDcQh5oZ03MfOmFEIRz5PMA0TGJ0d8CEmgBUy6wGh7zdGIpW3AaJi7hV5LfQ3s2MPR4NW4Px0rFA4sZStF1E2XExEmru7m7go5vP9FysXlOyquz6dNl95vXUR0jEjU9d0XOg8sRLGl88D436qi6XsuIYGAPpp-OpqjX~Whh5rr8n3U0QZbPGRgiVw__"
+}
+
+export default images
