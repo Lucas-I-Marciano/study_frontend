1433b7e966646667436b957e363ccc8c9d5e9b2b
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-04-14 17:48:21 -0300

    refactor: Create Footer component

--- petX/frontend/src/App.jsx
+++ petX/frontend/src/App.jsx
@@ -1,16 +1,14 @@
 import images from "./assets"
 import { Header } from "./components/Header"
 import { FormLogin } from "./components/FormLogin"
+import { Footer } from "./components/Footer"
 
 function App() {
   return (
     <>
       <Header login_back={images.login_back} icon={images.login_paw} />
       <FormLogin />
-      <div className="bg-white flex w-full items-center gap-5 justify-center pb-8">
-        <img src={images.login_paw_footer} alt="" />
-        <span className="text-principal font-bold text-xl font-[Poppins]">PETWITTER</span>
-      </div>
+      <Footer icon={images.login_paw_footer} textFooter="PETWITTER" />
 
     </>
   )
