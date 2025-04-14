52d63c8874b99c7fe9eaaeb1bc8cc93f04b548bb
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-04-13 23:07:10 -0300

    feat: Add background image with black transparency to login screen and divide layout into image, form, and footer

--- petX/frontend/src/App.jsx
+++ petX/frontend/src/App.jsx
@@ -1,7 +1,25 @@
+import images from "./assets"
+
 function App() {
 
   return (
-    <> My App </>
+    <>
+      <div className={`bg-black-100`}>
+        <div>
+          <img src={images.login_back} className="bg-black absolute -top-35 -z-2" alt="" />
+          <div className="bg-black h-full w-full absolute opacity-60 -top-60 -z-1"></div>
+        </div>
+        <div className="px-5 py-10">
+          <img src={images.login_paw} className="py-bottom-5" alt="" />
+          <p className="text-white">Comece agora.</p>
+          <p className="text-white">Conecte-se já.</p>
+        </div>
+
+      </div>
+      <div className="form bg-white h-20 w-full"> FORM</div>
+      <div className="form bg-white h-20 w-full"> Footer</div>
+
+    </>
   )
 }
 
