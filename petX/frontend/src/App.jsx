7cfad6a18a76c917ece4fe1aa71e9c85c226ae0b
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-04-14 18:03:24 -0300

    refactor: Move components from App.jsx to HomeLogin.jsx and create 'pages' folder

--- petX/frontend/src/App.jsx
+++ petX/frontend/src/App.jsx
@@ -1,15 +1,9 @@
-import images from "./assets"
-import { Header } from "./components/Header"
-import { FormLogin } from "./components/FormLogin"
-import { Footer } from "./components/Footer"
+import { HomeLogin } from "./pages/HomeLogin"
 
 function App() {
   return (
     <>
-      <Header login_back={images.login_back} icon={images.login_paw} />
-      <FormLogin />
-      <Footer icon={images.login_paw_footer} textFooter="PETWITTER" />
-
+      <HomeLogin />
     </>
   )
 }
